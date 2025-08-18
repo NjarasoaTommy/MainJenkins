5b8b5cb94a37cf72a7796f8b16cb9f7857e0f9ba
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-18 14:52:19 +0300

    Remove the 2 last print

--- com/integration/continue/Main.java
+++ com/integration/continue/Main.java
@@ -1,7 +1,5 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Test of github webhook");
-        System.out.println("The build didn(t began automatically)");
     }
 }
